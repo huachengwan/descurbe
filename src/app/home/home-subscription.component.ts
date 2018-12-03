48f0a86e39c5fcf6a0683daf78b092eabd45e434
CiGuan <43547041+CiGuan@users.noreply.github.com>
2018-12-03 22:34:33 +0800

    Update home-subscription.component.ts
    12-3-sub

--- src/app/home/home-subscription.component.ts
+++ src/app/home/home-subscription.component.ts
@@ -3,11 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-subscription',
   template: `
-    <p>
-      home-subscription works!
-    </p>
+  <div class="container">
+    <div class="row">
+      <p class="text-center title">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;"> Do not miss the best events in León, receive our Electronic Newsletter, totally free Do not miss the best events in León, receive our Electronic Newsletter, totally free</font>
+        </font>
+      </p>
+    </div>
+    <div class="row">
+        <div class="row susc-forma">
+            <input class="form-control col-md-9 mr-2" placeholder="tu@correo.com" type="text">
+            <button class="btn_suscribirme">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">Subscribe&gt;</font>
+                </font>
+            </button>
+        </div>
+    </div>
+  </div>
   `,
-  styles: []
+  styles: [`
+    .title {
+      width: 100%;
+      margin-inline-end: 313.5px; margin-inline-start: 313.5px;
+      margin-left: 313.5px; margin-right: 313.5px;
+    } 
+  `]
 })
 export class HomeSubscriptionComponent implements OnInit {
 
