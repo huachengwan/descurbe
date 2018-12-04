3e43f7b9b0dbd45836e138faab3df732b0d07d2f
CiGuan <43547041+CiGuan@users.noreply.github.com>
2018-12-04 22:53:48 +0800

    1204-22:56

--- src/app/home/event/archive/item.component.ts
+++ src/app/home/event/archive/item.component.ts
@@ -3,62 +3,107 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-event-archive-item',
   template: `
-    <div class="row card-agenda ng-star-inserted">
-      <div class="d-none d-lg-block col-lg-2 pl-0">
-        <div class="div-agenda-little-box ng-star-inserted">
-          <span class="div-agenda-little-box-t1">
-            From 
-          </span>
-          <span class="div-agenda-little-box-t2">
-            7:00 
-          </span>
-          <span class="div-agenda-little-box-t3">
-            AM.
-          </span>
-        </div>
-      </div>
-      <div class="col-12 d-block d-lg-none text-center">
-        <span class="span-responsive-agenda-little-text ng-star-inserted">
-          From 7:00 AM.
-        </span>
-      </div>
-      <div class="col-12 col-lg-5 img-container px-0">
-        <div class="evento-img-label" style="background-color: rgba(250, 123, 10, 0.8);">
-            Restaurants
-        </div>
-        <img class="img-fluid" src="assets/img/descubreleon/agenda/noviembre_files/rest_elfaro-desayuno.jpg">
-      </div>
-      <div class="col-12 col-lg-5 body cuerpo-text-card-container">
-        <div class="ctcc_1">
-          <h4>
-            2x1 buffet breakfast at El Faro restaurant
-          </h4>
-          <div class="subtitle">
-            of the Hotel Enterprise Inn
+    <div>
+      <div class="row box">
+        <div class="d-none d-lg-block col-lg-2">
+          <div class="timebox">
+            <span class="timeFrom">
+              From 
+            </span>
+            <span class="timeTime">
+              7:00 
+            </span>
+            <span class="timeAMPM">
+              AM.
+            </span>
           </div>
         </div>
-        <div class="ctcc_2">
-          <div class="cuerpo-text-card">
-            Do not forget to try their delicious bread fresh out of the oven
-          </div>
+        <div class="col-12 d-block d-lg-none text-center">
+          <span class="span-responsive-agenda-little-text">
+            From 7:00 AM.
+          </span>
         </div>
-        <div class="ctcc_3 text-right">
-          <button class="btn_agenda_miniaction ng-star-inserted">
-            <i class="fa fa-calendar">
-
-            </i>
+        <div class="col-12 col-lg-5 img-container px-0">
+          <div class="title" style="background-color: rgba(250, 123, 10, 0.8);">
+              Restaurants
+          </div>
+          <img class="img-fluid" src="assets/img/descubreleon/agenda/noviembre_files/nov01_altar.jpg">
+        </div>
+        <div class="col-12 col-lg-5 archiveContent">
+          <div class="mainTitle">
+            <h4>
+              2x1 buffet breakfast at El Faro restaurant
+            </h4>
+            <div class="subTitle">
+              of the Hotel Enterprise Inn
+            </div>
+          </div>
+          <div class="content">
+            <div class="">
+              Do not forget to try their delicious bread fresh out of the oven
+            </div>
+          </div>
+          <div class="ctcc_3 text-right">
+            <button class="btn_schedule">
+              <i class="fa fa-calendar">
+              </i>
               Schedule
-          </button>
-          <button class="btn_agenda_miniaction ng-star-inserted" tabindex="0">
-            See more&gt;
-          </button>
+            </button>
+            <button class="btn_seeall" tabindex="0">
+              See more&gt;
+            </button>
+          </div>
         </div>
       </div>
     </div>
 
   `,
   styles: [`
-    
+  .timebox {
+    padding: 5%;
+    background-color: rgb(233, 233, 233);
+  }
+  .timeTime {
+    font-size: 30px;
+    display: block;
+    font-weight: bold;
+  }
+  .timeFrom {
+    font-size: 13px;
+    display: block;
+  }
+  .timeAMPM {
+    font-size: 13px;
+    display: block;
+    font-weight: bold;
+  }
+  .box{
+    padding-left: 7%;
+    margin-top: 2%;
+  }
+  .img-container .title {
+    position: absolute;
+    left: 0; top: 0;
+    background-color: rgba(0,0,0,0.5);
+    font-size: 20px; color: #fff;
+  }
+  .archiveContent {
+    padding: 3%;
+  }
+  .content {
+    height: 40%;
+    overflow: hidden;
+  }
+  .archiveContent {
+    background-color: rgb(233, 233, 233)
+  }
+  button {
+    border: none;
+    background-color: rgb(176, 176, 176);
+    padding-right:5%; padding-left:5%; padding-top: 2%;padding-bottom: 2%;
+    margin-right: 2%;
+    color: rgb(255, 255, 255);
+  }
   `]
 })
 export class EventArchiveItemComponent implements OnInit {
