c8a155288f8fdf8a43ff9a7b8a527b71cef872ed
huachengwan <43547044+huachengwan@users.noreply.github.com>
2018-12-04 15:36:25 +0800

    1204-15:36

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,6 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth-guard.service';
 import { HomeComponent } from '../home/home.component';
 import { EventComponent } from '../home/event/event.component';
+import { AttractionComponent } from '../home/attraction/attraction.component';
+import { RestaurantComponent } from '../home/restaurant/restaurant.component';
+import { CafeComponent } from '../home/cafe/cafe.component';
+import { BarComponent } from '../home/bar/bar.component';
+import { HotelComponent } from '../home/hotel/hotel.component';
+import { BathComponent } from '../home/bath/bath.component';
 
 const routes: Routes = [{
   path: '',
@@ -12,7 +18,22 @@ const routes: Routes = [{
   component: EventComponent
 }, {
   path: 'attraction',
-  component: EventComponent
+  component: AttractionComponent
+}, {
+  path: 'restaurant',
+  component: RestaurantComponent
+}, {
+  path: 'cafe',
+  component: CafeComponent
+}, {
+  path: 'bar',
+  component: BarComponent
+}, {
+  path: 'hotel',
+  component: HotelComponent
+}, {
+  path: 'bath',
+  component: BathComponent
 }, {
   path: 'auth',
   loadChildren: 'app/auth/auth.module#AuthModule'
