f8d810f0ef8fe398216f11db3bb5c01d26c994e0
huachengwan <43547044+huachengwan@users.noreply.github.com>
2018-12-04 17:50:30 +0800

    1204-17:50

--- src/app/home/home-subscription.component.ts
+++ src/app/home/home-subscription.component.ts
@@ -4,62 +4,58 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home-subscription',
   template: `
   <div class="container">
-    <div class="row">
-      <p class="text-center title">
-        <font style="vertical-align: inherit;">
-          <font style="vertical-align: inherit;"> Do not miss the best events in León totally free Do not miss the best events in León, receive our Electronic Newsletter, totally free</font>
-        </font>
-      </p>
+    <div class="row desc">
+      <div class="col-md-6 col-sm-12">
+        <p class="text-center">
+          Do not miss the best events in León totally free Do not miss the best events in León, receive our Electronic Newsletter, totally free
+        </p>
+      </div>      
     </div>
-    <div class="row">
-      <div class="row submit">
+    <div class="row submit">
+      <div class="col">
         <input class="form-control col-md-9 mr-2" placeholder="tu@correo.com" type="text">
-        <button class="btn_suscribirme d-block d-md-none">
-          <font style="vertical-align: inherit;">
-            <font style="vertical-align: inherit;">&gt;</font>
-          </font>
-        </button>
-        <button class="btn_suscribirme d-none d-md-block">
-          <font style="vertical-align: inherit;">
-            <font style="vertical-align: inherit;">Subscribe&gt;</font>
-          </font>
+        <button type="submit" class="btn_suscribirme">
+          Subscribe&nbsp;&gt;
         </button>
       </div>
     </div>
   </div>
   `,
   styles: [`
-    .title {
+    .desc > div {
+      margin: 0 auto;
+    }
+    .desc p {
       width: 100%;
       font-size: 25px;
       color: rgb(14, 139, 47);
       font-family: Oswald;
-    } 
-    .submit {
-      width: 100%;
-      margin-top: 0px;
-      margin-bottom: 2%;
-      padding-left: 15%;
     }
-    .form-control {
-      max-width: 40%;
-      min-width: 150px;
+    .submit > div {
+      margin: 0 auto;
+      text-align: center;
     }
-    .btn_suscribirme {
-      width: 40%;
-      background-color: rgb(14, 139, 47);
-      color: #fff;
+    .submit input {
+      width: 220px;
+      display: inline-block;
+    }
+    .submit button[type=submit] {
+      width: 150px;
+      display: inline-block;
       cursor: pointer;
-      font-size: 20px;
-      line-height: 35px;
+      background-color: rgb(14, 139, 47);
+      color: #fff; font-size: 20px; line-height: 35px;
       border: 0;
-      min-width: 150px;
     }
-    .container {
-      width: 31%;
-      margin-top: 2%;
-      margin-bottom: 2%;
+    @media(max-width: 767.98px) {
+      .submit input {
+        width: 100%;
+      }
+      .submit button[type=submit] {
+        width: 100%;
+      }
     }
+    
   `]
 })
 export class HomeSubscriptionComponent implements OnInit {
