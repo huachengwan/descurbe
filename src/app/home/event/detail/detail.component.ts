78ee658d688e3e21c04e2f6be37ee70807835048
huachengwan <43547044+huachengwan@users.noreply.github.com>
2018-12-05 14:29:23 +0800

    1205-14:29

--- src/app/home/event/detail/detail.component.ts
+++ src/app/home/event/detail/detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { ArticleService } from '../../../service/article.service';
 import { ItemOfEvent } from '../../../service/item-of-event';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-event-detail',
@@ -11,12 +12,20 @@ import { ItemOfEvent } from '../../../service/item-of-event';
 })
 export class EventDetailComponent implements OnInit {
   itemData: ItemOfEvent;
+  itemDataDescHtml: any;
   constructor(
     private route: ActivatedRoute,
-    private articleService: ArticleService
+    private articleService: ArticleService,
+    private http: HttpClient
   ) {
     let link = '/event/'+route.snapshot.params['link'];
     this.itemData = articleService.getOneEvent(link);
+    /*
+    http.get(this.itemData.desc_html_file).subscribe(res => { 
+      this.itemDataDescHtml = res;
+      console.log(this.itemDataDescHtml);
+    });
+    */
   }
 
   ngOnInit() {
